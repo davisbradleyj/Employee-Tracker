73850671b4b1a6823feb5eb0d52e39e0fda23105
guachochi <davis.bradleyj@gmail.com>
2020-04-18 15:59:55 -0700

    add functions: view dept, role, empl; add dept, role, empl

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-
+// establish dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+// establish connection
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -11,4 +13,144 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  start();
 });
+
+function start() {
+    inquirer.prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: ["Add a department", 
+      "Add a role", 
+      "Add an employee", 
+      "View departments", 
+      "View roles", 
+      "View employees", 
+      "Update employee role",
+      "Exit"
+      ]
+    })
+    .then(function(answer){
+        switch (answer.action) {
+            case "Add a department":
+              addDepartment();
+              break;
+      
+            case "Add a role":
+              addRole();
+              break;
+      
+            case "Add an employee":
+              addEmployee();
+              break;
+      
+            case "View departments":
+              viewDepartments();
+              break;
+      
+            case "View roles":
+              viewRoles();
+              break;
+
+            case "View employees":
+              viewEmployees();
+              break; 
+
+            case "Update employee role":
+              updateEmployeeRoles();
+              break;
+            
+            case "Exit":
+              exit();
+              break; 
+        }
+    });
+}
+
+// build exit function to end session in app
+function exit() {
+    console.log("Thank you for time, and have a nice day!")
+    connection.end();
+}
+
+// build view department, selecting all from dept. table
+// display using console.table dependency
+function viewDepartments() {
+    var query = "SELECT * FROM department"
+    connection.query(query, function(err,res) {
+        if (err) throw err;
+        console.table(res);
+    })
+    start(); 
+};
+
+// build view role, selecting all from role table
+// display using console.table dependency
+function viewRoles() {
+    var query = "SELECT * FROM role"
+    connection.query(query, function(err,res) {
+        if (err) throw err;
+        console.table(res);
+    })
+    start();
+}; 
+
+// build view employee, selecting all from employee table
+// display using console.table dependency
+function viewEmployees() {
+    var query = "SELECT * FROM employee"
+    connection.query(query, function(err,res) {
+        if (err) throw err;
+        console.table(res);
+    })
+    start();
+}; 
+
+// add department
+function addDepartment() {
+    inquirer
+      .prompt({
+        name: "dept",
+        type: "input",
+        message: "What department would you like to add?"
+      })
+      .then(function(answer) {
+        console.log(answer)
+        // var query = "INSERT INTO department (name) VALUES (?)";
+        // connection.query(query, answer.dept, function(err,res){
+        //     if (err) throw err
+        })
+        start();
+    // });
+}
+
+//add role
+function addRole() {
+    inquirer
+      .prompt({
+        name: "title",
+        type: "input",
+        message: "What role would you like to add?"
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the salary for that role?"
+      },
+      {
+        name: "dept_id",
+        type: "input",
+        message: "What department is it part of?"
+      },
+      )
+      .then(function(answer) {
+        console.log(answer)
+        // var query = "INSERT INTO role (title,salary,dept_id) VALUES (?)";
+        // connection.query(query, answer, function(err,res){
+        //     if (err) throw err
+        })
+        start();
+    // });
+}
--- seed.sql
+++ seed.sql
@@ -10,55 +10,55 @@ INSERT INTO department (name)
 VALUES ("Finance");
 INSERT INTO department (name)
 VALUES ("Human Resources") ;
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Web Developer",100000,1);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Software Engineer",120000,1);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Lead Engineer",150000,1);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Chief Technology Officer",200000,1);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Sales Development Rep",60000,2);
-INSERT INTO role (title, salary, department_id) 
+INSERT INTO role (title, salary, dept_id) 
 VALUES ("Account Executive",85000,2);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Sales Director",120000,2);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Vice President - Sales",160000,2);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Chief Revenue Officer",200000,2);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Compliance Analyst",100000,3);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Compliance Manager",130000,3);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Counsel",180000,3);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Chief Counsel",220000,3);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("IT Analyst",90000,4);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("IT Manager",130000,4);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Finance Operations Analyst",80000,5);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Finance Operations Manager",120000,5);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Accountant",95000,5);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Controller",140000,5);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Risk Analyst",120000,5);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Risk Manager",160000,5);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Chief Financial Officer",200000,5);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Human Resources Analyst",80000,6);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Human Resources Manager",120000,6);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (title, salary, dept_id)
 VALUES ("Chief Human Resources Officer",180000,6);
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("Everett", "Begum",2);
