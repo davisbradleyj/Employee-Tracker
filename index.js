3c6068de200e6714ea78561ce05589176580b39f
guachochi <davis.bradleyj@gmail.com>
2020-04-19 17:22:31 -0700

    modify schema; adjust index.js inquirer menus to group by dept role and employee

--- index.js
+++ index.js
@@ -16,142 +16,297 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId + "\n");
   start();
 });
-
+//main menu
 function start() {
-    inquirer.prompt([
-    {
-      name: "action",
-      type: "rawlist",
-      message: "What would you like to do?",
-      choices: ["Add a department", 
-      "Add a role", 
-      "Add an employee", 
-      "View departments", 
-      "View roles", 
-      "View employees", 
-      "Update employee role",
-      "Exit"
-      ]
+  inquirer.prompt([
+   {
+    name: "action",
+    type: "rawlist",
+    message: "What would you like to do?",
+    choices: ["Review Departments", 
+    "Review Roles", 
+    "Review Employees", 
+    "Exit"]
     }])
-    .then(function(answer){
-        switch (answer.action) {
-            case "Add a department":
-              addDepartment();
-              break;
+  .then(function(answer){
+      switch (answer.action) {
+          case "Review Departments":
+            dept();
+            break;
       
-            case "Add a role":
-              addRole();
-              break;
+          case "Review Roles":
+            roles();
+            break;
       
-            case "Add an employee":
-              addEmployee();
-              break;
+          case "Review Employees":
+            employees();
+            break;
       
-            case "View departments":
-              viewDepartments();
-              break;
-      
-            case "View roles":
-              viewRoles();
-              break;
-
-            case "View employees":
-              viewEmployees();
-              break; 
-
-            case "Update employee role":
-              updateEmployeeRoles();
-              break;
-            
-            case "Exit":
-              exit();
-              break; 
-        }
-    });
+          case "Exit":
+            exit();
+            break; 
+      }
+  });
 }
+// department menu
+function dept() {
+  inquirer.prompt([
+  {
+    name: "action",
+    type: "rawlist",
+    message: "What would you like to do?",
+    choices: ["View Departments", 
+    "Add a Department", 
+    "Delete Departments",
+    "View Department Budget", 
+    "Return to Start Menu"
+    ]
+  }
+  ])
+  .then(function(answer){
+      switch (answer.action) {
+          case "View Departments":
+            viewDepartments();
+            break;
+    
+          case "Add a Department":
+            addDepartment();
+            break;
+    
+          case "Delete a Department":
+            delDepartment();
+            break;
+          
+          case "View Departments Budget":
+            deptBudget();
+            break;  
 
+          case "Return to Start Menu":
+            start();
+            break; 
+      }
+  });
+}
+// roles menu
+function roles() {
+  inquirer.prompt([
+  {
+    name: "action",
+    type: "rawlist",
+    message: "What would you like to do?",
+    choices: ["View Roless", 
+    "Add a Role", 
+    "Delete Role", 
+    "Return to Start Menu"
+    ]
+  }
+  ])
+  .then(function(answer){
+      switch (answer.action) {
+          case "View Roles":
+            viewRoles();
+            break;
+    
+          case "Add a Role":
+            addRole();
+            break;
+    
+          case "Delete a Role":
+            delRole();
+            break;
+          
+          case "Return to Start Menu":
+            start();
+            break; 
+      }
+  });
+}
+// employees menu
+function employees() {
+  inquirer.prompt([
+  {
+    name: "action",
+    type: "rawlist",
+    message: "What would you like to do?",
+    choices: ["View Employees", 
+    "Add an Employee", 
+    "Delete an Employee",
+    "Update Manager",
+    "View Employees by Manager",
+    "Return to Start Menu"
+    ]
+  }
+  ])
+  .then(function(answer){
+    switch (answer.action) {
+        case "View Employees":
+          viewEmployees();
+          break;
+  
+        case "Add an Employee":
+          addEmployee();
+          break;
+  
+        case "Delete an Employee":
+          delEmployee();
+          break;
+        
+        case "Update Manager":
+          updateManager();
+          break;  
+        
+        case "View Employees by Manager":
+          managerEmployees();  
+          break;
+
+        case "Return to Start Menu":
+          start();
+          break; 
+    }
+  });
+}
 // build exit function to end session in app
 function exit() {
-    console.log("Thank you for time, and have a nice day!")
-    connection.end();
+  console.log("Thank you for your time, and have a nice day!")
+  connection.end();
 }
-
 // build view department, selecting all from dept. table
 // display using console.table dependency
 function viewDepartments() {
-    var query = "SELECT * FROM department"
-    connection.query(query, function(err,res) {
-        if (err) throw err;
-        console.table(res);
-    })
-    start(); 
+  var query = "SELECT * FROM department"
+  connection.query(query, function(err,res) {
+      if (err) throw err;
+      console.table(res);
+      start(); 
+  })
 };
-
 // build view role, selecting all from role table
 // display using console.table dependency
 function viewRoles() {
-    var query = "SELECT * FROM role"
-    connection.query(query, function(err,res) {
-        if (err) throw err;
-        console.table(res);
-    })
-    start();
+  var query = "SELECT * FROM role"
+  connection.query(query, function(err,res) {
+      if (err) throw err;
+      console.table(res);
+      start();
+  })    
 }; 
-
 // build view employee, selecting all from employee table
 // display using console.table dependency
 function viewEmployees() {
-    var query = "SELECT * FROM employee"
-    connection.query(query, function(err,res) {
-        if (err) throw err;
-        console.table(res);
-    })
-    start();
+  var query = "SELECT * FROM employee"
+  connection.query(query, function(err,res) {
+      if (err) throw err;
+      console.table(res);
+      start();
+  })
 }; 
-
-// add department
+// add department, not writing as of 4-19-2020
 function addDepartment() {
-    inquirer
-      .prompt([
-      {
-        name: "dept",
-        type: "input",
-        message: "What department would you like to add?"
-      }])
-      .then(function(answer) {
-        console.log(answer)
-        // var query = "INSERT INTO department (name) VALUES (?)";
-        // connection.query(query, answer.dept, function(err,res){
-        //     if (err) throw err
-        start();
-        })
-    // });
+  inquirer
+    .prompt([
+    {
+      name: "dept",
+      type: "input",
+      message: "What department would you like to add?"
+    }])
+    .then(function(answer) {
+      console.log(answer.dept)
+      // var query = "INSERT INTO department (name) VALUES (?)";
+      // connection.query(query, answer.dept, function(err,res){
+      //     if (err) throw err
+      viewDepartments();
+      })
+  // });
+}
+//add role, not writing as of 4-19-2020
+function addRole() {
+  inquirer.prompt([
+    {
+      name: "title",
+      type: "input",
+      message: "What role would you like to add?"
+    },
+    {
+      name: "salary",
+      type: "input",
+      message: "What is the salary for that role?"
+    },
+    {
+      name: "dept_id",
+      type: "input",
+      message: "What department is it part of?"
+    }])
+    .then(function(answer) {
+      console.log(answer)
+      // var query = "INSERT INTO role (title,salary,dept_id) VALUES (?)";
+      // connection.query(query, answer, function(err,res){
+      //     if (err) throw err
+      viewRoles();
+      })
+  // });
+}
+//add employee, not writing as of 4-19-2020
+function addEmployee() {
+  inquirer.prompt([
+    {
+      name: "first_name",
+      type: "input",
+      message: "What is the first name?"
+    },
+    {
+      name: "last_name",
+      type: "input",
+      message: "What is the last name?"
+    },
+    {
+      name: "role_id",
+      type: "input",
+      message: "What is the role id?"
+    },
+    {
+      name: "manager_id",
+      type: "input",
+      message: "Please enter the manager id, if applicable."
+    }])
+    .then(function(answer) {
+      console.log(answer)
+      // var query = "SELECT * from employee (first_name,last_name,role_id,manager_id) VALUES (?)";
+      // connection.query(query, answer, function(err,res){
+      //     if (err) throw err
+      viewEmployees();
+      })
+  // });
+}
+// update manager function, not writing as of 4-19-2020
+function updateManager() {
+  inquirer.prompt([       
+    {
+      name: "employee_id",
+      type: "input",
+      message: "What is the employee id number?"
+    },
+    {
+      name: "manager_id",
+      type: "input",
+      message: "What is the manager's employee id number?"
+    }])
+    .then(function(answer) {
+      console.log(answer)
+      // var query = "INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES (?)";
+      // connection.query(query, answer, function(err,res){
+      //     if (err) throw err
+      viewEmployees();
+  })
 }
 
-//add role
-function addRole() {
-    inquirer.prompt([
-      {
-        name: "title",
-        type: "input",
-        message: "What role would you like to add?"
-      },
-      {
-        name: "salary",
-        type: "input",
-        message: "What is the salary for that role?"
-      },
-      {
-        name: "dept_id",
-        type: "input",
-        message: "What department is it part of?"
-      }])
-      .then(function(answer) {
-        console.log(answer)
-        // var query = "INSERT INTO role (title,salary,dept_id) VALUES (?)";
-        // connection.query(query, answer, function(err,res){
-        //     if (err) throw err
-        start();
-        })
-    // });
-}
+// Review Dept budget
+// console.table a join which displays totals for each depts salaries
+
+// View employees by Manager number
+// console.table a join which displays all employees under a certain manager number
+
+// Delete a Department
+
+// Delete a Role
+
+// Delete an Employee
--- schema.sql
+++ schema.sql
@@ -3,19 +3,19 @@ CREATE database employeeTracker;
 USE employeeTracker;
 
 CREATE TABLE department (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+dept_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) 
 );
 CREATE TABLE role (
-id VARCHAR(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL(10,2) NOT NULL,
-department_id INT REFERENCES department(department_id)
+department_id INT REFERENCES department(dept_id)
 );
 CREATE TABLE employee (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL REFERENCES role(role_id),
-manager_id INT REFERENCES employee(employee_id)
+manager_id INT REFERENCES employee(emp_id)
 );
